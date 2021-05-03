39e57565ed0a0705e675392949251347769f5308
grayfallstown <grayfallstown@protonmail.com>
2021-05-03 12:20:19 +0200

    no longer publishing as zip as the zip is blocked by browsers and the setup is not

--- release.ps1
+++ release.ps1
@@ -5,4 +5,4 @@ mkdir release\ChiaPlotStatus\
 
 copy -r C:\Users\mk\IdeaProjects\ChiaPlotStatus\bin\Release\net5.0\* release\ChiaPlotStatus\
 rm release\ChiaPlotStatus\ChiaPlotStatus.dll.config.json
-Compress-Archive release\ChiaPlotStatus release\ChiaPlotStatus.zip
+# Compress-Archive release\ChiaPlotStatus release\ChiaPlotStatus.zip
