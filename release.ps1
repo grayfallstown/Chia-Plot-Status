f3bc6f48ab3bcdacc5c993034abdece2498fa60b
grayfallstown <grayfallstown@protonmail.com>
2021-05-11 22:57:01 +0200

    fixed release.ps1

--- release.ps1
+++ release.ps1
@@ -1,7 +1,7 @@
 Echo "Set Version!"
 pause
 
-dotnet publish win-x64 --configuration .\ChiaPlotStatus.sln /p:Configuration=Release /p:Platform="Any CPU"
+dotnet publish -r win-x64 --configuration .\ChiaPlotStatus.sln /p:Configuration=Release /p:Platform="Any CPU"
 
 Echo ""
 Echo "The warnings are normal. Only formal flaws in the code"
@@ -14,8 +14,8 @@ mkdir release\
 mkdir release\ChiaPlotStatus\
 
 Echo "Copying build to release folder"
-xcopy /sy C:\Users\mk\IdeaProjects\ChiaPlotStatus\ChiaPlotStatusLib\bin\Release\net5.0\* release\ChiaPlotStatus\
-xcopy /sy C:\Users\mk\IdeaProjects\ChiaPlotStatus\ChiaPlotStatusGUI\bin\Release\net5.0\* release\ChiaPlotStatus\
-xcopy /sy C:\Users\mk\IdeaProjects\ChiaPlotStatus\ChiaPlotStatusCli\bin\Release\net5.0\* release\ChiaPlotStatus\
+xcopy /sy C:\Users\mk\IdeaProjects\ChiaPlotStatus\ChiaPlotStatusLib\bin\Release\net5.0\win-x64\publish\* release\ChiaPlotStatus\
+xcopy /sy C:\Users\mk\IdeaProjects\ChiaPlotStatus\ChiaPlotStatusGUI\bin\Release\net5.0\win-x64\publish\* release\ChiaPlotStatus\
+xcopy /sy C:\Users\mk\IdeaProjects\ChiaPlotStatus\ChiaPlotStatusCli\bin\Release\net5.0\win-x64\publish\* release\ChiaPlotStatus\
 
 Echo "Now open and build setup using InstallForge and InstallerConfig.ifp as config"
